3d52252e62cfbcd2d5a5915896962bdd5e3b76d0
Kevin Pocon <inkev.ap@gmail.com>
2022-09-10 21:20:31 -0600

    Ejercicio 3

---
+++ Tareas/Tarea1/Ejercico3/src/main/java/com/Tarea1/Ejercico3/Ejercico3Application.java
@@ -0,0 +1,13 @@
+package com.Tarea1.Ejercico3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercico3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercico3Application.class, args);
+	}
+
+}
---
+++ Tareas/Tarea1/Ejercico3/src/test/java/com/Tarea1/Ejercico3/Ejercico3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea1.Ejercico3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercico3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
